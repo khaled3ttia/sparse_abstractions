90d7c11c8e3190de8a2367df51d4091a276f5cc5
khaled3ttia <khaled.3ttia@gmail.com>
2021-06-16 22:36:30 -0500

    basic cache classes and implementation

---
+++ blockcache.h
@@ -0,0 +1,78 @@
+#include <map>
+
+enum replacementPolicy {
+    FIFO,
+    LRU
+};
+
+struct BlockId{
+    int matId; 
+    int blockId;
+};
+
+struct IDCompare{
+    bool operator() (const BlockId& lhs, const BlockId& rhs){
+        return ((lhs.matId < rhs.matId) || ((lhs.matId == rhs.matId) && (lhs.blockId < rhs.blockId)));
+    }
+};
+
+
+template<typename T>
+class Block{
+    int _size;
+    T* _data;
+    bool _dataAllocated = false;
+    int _accessCount = 0;
+    int _insertTime;
+
+    public:
+    Block(){}; 
+    Block(T*&);
+    Block(T*&, int);
+    void setSize(int);
+    int getSize(); 
+    void access();
+    void setInsertTime(int);
+    int getInsertTime();
+
+    // Not safe? 
+    T*& getData();
+
+    T& operator [](int);
+};
+
+template<typename T>
+class BlockCache{
+    
+    // A counter to capture number of insertions
+    // this is also propagated to each individual block
+    // to serve as an insertion timestamp for the block   
+    int _insertions;
+
+    // Cache size in BLOCKS
+    // TODO: change to KB ? 
+    int _size = 32;
+
+    // Cache replacement policy
+    replacementPolicy _rPolicy = FIFO;
+
+    // The actual cache
+    std::map<BlockId, Block<T>, IDCompare> _cache;
+
+    public:
+
+    BlockCache(){};
+
+    BlockCache(int);
+
+    BlockCache(int, replacementPolicy);
+
+    bool insert(BlockId, Block<T>);
+
+    size_t remove(BlockId);
+
+    // Not safe?
+    T*& access(BlockId);
+};
+
+#include "blockcache_impl.h"
---
+++ blockcache_impl.h
@@ -0,0 +1,125 @@
+template<typename T>
+BlockCache<T>::BlockCache(int size):_size(size){}
+
+template<typename T>
+BlockCache<T>::BlockCache(int size, replacementPolicy rPolicy): _size(size), _rPolicy(rPolicy){}
+
+template<typename T>
+bool BlockCache<T>::insert(BlockId bid, Block<T> block){
+    
+    assert(_size > 0);
+    
+
+    // Record insetion order 
+    _insertions++; 
+
+    block.setInsertTime(_insertions); 
+
+    if (_cache.size() < _size){
+         _cache.insert(std::make_pair(bid, block));
+         return true;
+    }else {
+        
+        // TODO: implement other replacement policies here
+                
+        auto oldest = _cache.begin();
+        auto it = std::next(oldest);
+
+        while (it != _cache.end()){
+            if (it->second.getInsertTime() < oldest->second.getInsertTime()){
+                oldest = it;
+            }
+            it++;
+        }
+        _cache.insert(std::make_pair(bid, block));
+        return true;
+    }
+    return false;
+}
+
+template<typename T>
+size_t BlockCache<T>::remove(BlockId bid){
+    return _cache.erase(bid);
+}
+
+template<typename T>
+T*& BlockCache<T>::access(BlockId bid){
+
+    auto it = _cache.find(bid);
+
+    if (it != _cache.end()){
+        
+        it->second.access();
+
+        return it->second.getData();
+    }
+
+}
+
+template<typename T>
+Block<T>::Block(T*& src){
+    
+    if (!_dataAllocated){
+        _data = new T[_size];
+        _dataAllocated = true;
+    }
+
+    for (int i = 0; i < _size; i++){
+        _data[i] = src[i];
+    }
+}
+
+template<typename T>
+Block<T>::Block(T*& src, int size){
+    _size = size;
+    if (!_dataAllocated){
+        _data = new T[size];
+        _dataAllocated = true;
+    }
+    for (int i = 0; i < size; i++){
+        _data[i] = src[i];
+    }
+}
+
+template<typename T>
+void Block<T>::setSize(int val){
+    assert(val > 0);
+    _size = val;
+}
+
+template<typename T>
+int Block<T>::getSize(){
+    return _size;
+}
+
+
+template<typename T>
+void Block<T>::access() {
+    _accessCount++; 
+}
+
+template<typename T>
+T*& Block<T>::getData(){
+    return _data;
+}
+
+template<typename T>
+void Block<T>::setInsertTime(int val){
+    assert(val > 0);
+    _insertTime = val;
+}
+
+template<typename T>
+int Block<T>::getInsertTime(){
+    return _insertTime;
+}
+
+template<typename T>
+T& Block<T>::operator [](int idx){
+    
+    assert(idx < _size);
+
+    assert(_dataAllocated);
+
+    return _data[idx];
+}
